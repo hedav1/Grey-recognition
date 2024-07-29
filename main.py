e6408eb9bc9df77d6391201bcb2fcb635941da81
hedav1 <134723915+hedav1@users.noreply.github.com>
2024-07-29 21:57:45 +0100

    Can now place circle on image
    On double click, large circle is placed.

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 
 
+
 baseImg = cv2.imread('liquid.jpg')
 gray = cv2.cvtColor(baseImg, cv2.COLOR_BGR2GRAY)
 
-def drawCircle(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(baseImg, (x, y), 100, (0, 255, 255), 2)
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(baseImg, (x, y), 100, (0, 255, 255), -1)
 
 cv2.namedWindow("Working Window")
 
-cv2.setMouseCallback("Working Window", drawCircle)
+cv2.setMouseCallback("Working Window", draw_circle)
 
 while True:
-    cv2.imshow("Working Window", gray)
-
-    if cv2.waitKey(0):
+    cv2.imshow("Working Window", baseImg)
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cv2.destroyAllWindows()
